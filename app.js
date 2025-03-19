79e87cfe5122a751f58eb72163c81ec8c09fd406
Rakshitha12B <Rakshitha@12B@gmail.com>
2025-03-19 11:34:42 +0530

    final change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const{User}= require (`./model/User`);
 const mongoose = require('mongoose');
 const cors =require('cors');
 const morgan =require('morgan');
-const bcrypt=require('bcrypt');
+const bcrypt=require('bcryptjs');
 const jwt =require('jsonwebtoken');
 const{Product}=require('./model/Product');
 const {Cart}=require ('./model/Cart');
@@ -14,9 +14,11 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 
+//7cbAwer6L9Nz2eUf
 
+let MONGODB_URL ="mongodb+srv://rakshithab377:7cbAwer6L9Nz2eUf@cluster0.ovdqc.mongodb.net/?retryWrites=true&w=majority"
 
-mongoose.connect('mongodb://127.0.0.1:27017/kleProject')
+mongoose.connect(MONGODB_URL)
 .then(()=>{
     console.log("DB is connected")
 }).catch((err)=>{
